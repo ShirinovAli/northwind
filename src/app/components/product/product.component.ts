0246584c8e6026a660fec6ad067d3626695797a0
ali-shirinov@hotmail.com <ali-shirinov@hotmail.com>
2021-03-14 23:15:40 +0400

    14.03.21 end day

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
+import { importType } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +10,18 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  constructor() {}
+  dataLoaded = false;
 
-  ngOnInit(): void {}
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
 }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // api call
+import { Observable } from 'rxjs';
+import { Todo } from '../models/todo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+  constructor(private httpClient: HttpClient) {}
+
+  getTodos(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>(this.apiUrl);
+  }
+}
