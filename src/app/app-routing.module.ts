e263e3ad2346d9118ccf52b9913bcaccd7deb2a5
ali-shirinov@hotmail.com <ali-shirinov@hotmail.com>
2021-03-25 20:40:15 +0400

    25.03.2021 commit 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'products', component: ProductComponent },
   { path: 'categories', component: CategoryComponent },
   { path: 'products/category/:categoryId', component: ProductComponent },
+  { path: 'products/add', component: ProductAddComponent },
 ];
 
 @NgModule({
